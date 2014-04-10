3374badf7c674fd703214272826274efdcf7b8c3
maddis reitav <maddis.reitav@gmail.com>
2014-04-10 11:49:14 +0300

    Script for disconnecting comment wiht participant

---
+++ actions/disconnect_comment_with_participant.php
@@ -0,0 +1,35 @@
+<?php
+
+    //Gatekeeper
+    gatekeeper();
+    action_gatekeeper();
+	
+	$course_guid = (int) get_input('course_guid');
+	$comment_id = (int) get_input('comment_id');
+	$participant_id = (int) get_input('participant_id');
+	$post_id = (int) get_input('post_id');
+	
+
+	$educourse = get_entity($course_guid);
+	
+	
+	if ($educourse->getSubtype() == 'educourse' && $educourse->canEdit() && edufeedrCanEditEducourse($educourse) && $comment_id && $participant_id) {
+		
+		
+		$es = new EduSuckr();
+		$result = $es->disconnectCommentWithParticipant($course_guid, $comment_id, $participant_id);
+
+		if ($result) {
+			/*translation:Comment connected.*/
+			system_message(elgg_echo('edufeedr:message:comment_disconnected'));
+		} else {
+			/*translation:Comment could not be connected.*/
+			register_error(elgg_echo('edufeedr:error:post_not_disconnected'));
+		}
+		// XXX WRONG URL, SHOULD BE POST
+		forward("pg/edufeedr/view_post/$course_guid/$post_id");
+		//DOTO POST_ID
+	}
+
+	forward('pg/edufeedr/');
+?>
